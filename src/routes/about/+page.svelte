ecba522756fa4d458cf36002c14136a85c2ecfc5
04nada <114312907+04nada@users.noreply.github.com>
2025-07-01 16:10:26 +0800

    feat: add About page contents

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,3 +3,48 @@
 </script>
 
 <Nav />
+
+<div>
+    <p>
+        The Office for Student Ethics is a disciplinary body formed to help promote ethical
+        student behavior and the values of Honor and Integrity as necessary complements
+        to the pursuit of Academic Excellence, Social Responsibility, and Personal Development.
+
+        Formerly the Student Disciplinary Council (SDC) formed by the Chancellor under the
+        administrative supervision of the Vice Chancellor for Student Affairs,the OSE is the
+        disciplinary arm of the University, as per the 2012 UP Diliman Code of Student Conduct.
+    </p>
+    
+    <h2> Services </h2>
+    <ul>
+        <li> Receive copies of all complaints and case reports involving student misconduct </li>
+        <li> Facilitate alternative dispute resolution (ADR) methods in cases involving less serious misconduct committed outside the jurisdiction of a college. </li>
+        <li> Form ad hoc committees that will hear and resolve cases involving serious misconduct, except intellectual dishonesty, and less serious misconduct involving more than one college. </li>
+        <li> Monitor the progress and resolution of all cases, including enforcement of corrective measures, and submit status reports to the Chancellor. </li>
+        <li> Accept and process requests for student disciplinary clearance. </li>
+    </ul>
+
+    <h2> History </h2>
+    (todo)
+
+    <div>
+        <div>
+        </div>
+        <div>
+            <h2> Vision </h2>
+            <p>
+                A safe and nurturing University environment where students learn civility, veer away
+                from destructive behavior, develop nationalism and social responsibility, and thrive
+                in a community of scholars that uphold the highest standards of honor and excellence.
+            </p>
+            
+            <h2> Mission </h2>
+            <ul>
+                <li> Orient faculty and students about the relevance of ethical conduct to academic and personal life. </li>
+                <li> Ensure fair, just, and effective promotion of ethical behavior, moral consciousness, peaceful management of conflicts, and enforcement of principled discipline. </li>
+                <li> nitiate review of the Code as a living document. </li>
+                <li> Facilitate the development of students' social and behavioral skills that would help them excel in a globally competitive environment. </li>
+            </ul>
+        </div>
+    </div>
+</div>
