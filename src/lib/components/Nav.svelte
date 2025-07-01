779815aadbe24704e8c3dc8bc9db322c9e1b9f16
04nada <114312907+04nada@users.noreply.github.com>
2025-07-01 15:09:04 +0800

    feat: add navbar endpoints

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,12 +1,12 @@
-<script> 
+<script lang=ts>
     import NavItem from "./NavItem.svelte";
 </script>
 
 <nav>
-    <NavItem navName="Home" route=""/>
-    <NavItem navName="News" route=""/>
-    <NavItem navName="Resources" route=""/>
-    <NavItem navName="Forms" route=""/>
-    <NavItem navName="About Us" route=""/>
-    <NavItem navName="Contact Us" route=""/>
+    <NavItem navName="Home" route="/"/>
+    <NavItem navName="News" route="/news/"/>
+    <NavItem navName="Resources" route="/resources/"/>
+    <NavItem navName="Forms" route="/forms/"/>
+    <NavItem navName="About Us" route="/about/"/>
+    <NavItem navName="Contact Us" route="/contact/"/>
 </nav>
