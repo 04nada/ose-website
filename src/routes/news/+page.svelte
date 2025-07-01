329c636fa7b3900624677a569ef5d6dfb32f72d5
04nada <114312907+04nada@users.noreply.github.com>
2025-07-01 15:14:07 +0800

    feat: add files for remaining pages

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,15 @@
+<script lang=ts>
+    import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<Nav />
+
+<div>
+    <h1> Contact Us </h1>
+
+    0981 850 0450 / 89818500 loc 4509
+
+    ose.upd@up.edu.ph
+
+    Address: Room 201, Office for Student Ethics, Vinzons Hall, Diliman, Quezon City, Metro Manila
+</div>
