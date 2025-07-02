ceda388587adb0be2f98f15e0e7f1411164526fd
04nada <114312907+04nada@users.noreply.github.com>
2025-07-02 14:11:35 +0800

    refactor: use form IDs/names for conditional rendering

--- src/routes/forms/+page.svelte
+++ src/routes/forms/+page.svelte
@@ -1,36 +1,50 @@
 <script lang=ts>
     import Nav from "$lib/components/Nav.svelte";
 
-    const forms = [
-        {
+    const forms = {
+        oseclearance: {
             title: 'Online Clearance Application Form',
-            embedSrc: 'https://docs.google.com/forms/d/e/1FAIpQLScFg0BzPqylk_lMs39rOCuZfoUJrf1e671Bbew7i1tA6eqOGA/viewform?embedded=true'
+            embedSrc: 'https://docs.google.com/forms/d/e/1FAIpQLScFg0BzPqylk_lMs39rOCuZfoUJrf1e671Bbew7i1tA6eqOGA/viewform?embedded=true',
+            publicLink: 'https://bit.ly/oseclearance'
         },
-        {
+        osedocumentrequest: {
             title: 'Request for Hard Copy of Documents',
-            embedSrc: 'https://docs.google.com/forms/d/e/1FAIpQLSfjaaLJXyCxvJsoPg7xMFh8MtqhMGLg_NYiFCmtdR_YFhMPbg/viewform?embedded=true'
+            embedSrc: 'https://docs.google.com/forms/d/e/1FAIpQLSfjaaLJXyCxvJsoPg7xMFh8MtqhMGLg_NYiFCmtdR_YFhMPbg/viewform?embedded=true',
+            publicLink: 'https://bit.ly/osedocumentrequest'
         }
-    ];
+    };
 
-    let selectedForm = $state(0);
+    let selectedForm = $state('oseclearance');
 </script>
 
 <Nav />
 
-{#each forms as form, i}
+{#each Object.entries(forms) as [id, form]}
 <button
-    onclick={() => selectedForm = i}
+    onclick={() => selectedForm = id}
 >
     {form.title}
 </button>
 {/each}
 
-<!-- todo: find a way to dynamically choose what to embed (not just an iframe) -->   
-<iframe
-    src={forms[selectedForm].embedSrc}
-    width=640 height=800
-    frameborder="0" marginheight="0" marginwidth="0"
-    title={forms[selectedForm].title}
->
-    Loading...
-</iframe>
+{#if selectedForm == 'oseclearance'}
+    <iframe
+        src={forms[selectedForm].embedSrc}
+        width=640 height=500
+        frameborder="0" marginheight="0" marginwidth="0"
+        title={forms[selectedForm].title}
+    >
+        Loading...
+    </iframe>
+{:else if selectedForm == 'osedocumentrequest'}
+    <iframe
+        src={forms[selectedForm].embedSrc}
+        width=640 height=500
+        frameborder="0" marginheight="0" marginwidth="0"
+        title={forms[selectedForm].title}
+    >
+        Loading...
+    </iframe>
+{:else}
+    <p> Invalid form selected. </p>
+{/if}
