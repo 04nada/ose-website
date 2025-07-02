0d95213ced15baa5a37fc60c8ca4060d62fd3ac9
04nada <114312907+04nada@users.noreply.github.com>
2025-07-02 14:55:00 +0800

    feat: embed resources

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,5 +1,36 @@
 <script lang=ts>
     import Nav from "$lib/components/Nav.svelte";
+
+    const resources = {
+        studentconduct2012: {
+            title: '2012 Code of Student Conduct',
+            embedSrc: 'https://drive.google.com/file/d/1jAakJ1bnbZ1f-CMwKdkGak-SwC4tpl0S/preview',
+            publicLink: 'https://rb.gy/iaod51'
+        },
+    };
+
+    let selectedResource = $state('studentconduct2012');
 </script>
 
 <Nav />
+
+{#each Object.entries(resources) as [id, resource]}
+<button
+    onclick={() => selectedResource = id}
+>
+    {resource.title}
+</button>
+{/each}
+
+{#if selectedResource == 'studentconduct2012'}
+    <iframe
+        src={resources[selectedResource].embedSrc}
+        width=640 height=800
+        frameborder="1" marginheight="0" marginwidth="0"
+        title={resources[selectedResource].title}
+    >
+        Loading...
+    </iframe>
+{:else}
+    <p> Invalid resource selected. </p>
+{/if}
