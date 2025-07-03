317dbf61f0b71c30f5703e511984121280966b2e
sojaja <janmend3@gmail.com>
2025-07-03 16:38:36 +0800

    refactor: logo padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
   class="min-h-screen w-full bg-cover bg-center bg-fixed relative"
   style="background-image: url('/ose-bg.png')"
 >
-  <div class="pt-12 pl-25">
+  <div class="pt-5 pl-25">
     <img
       src="/ose-logo.png"
       alt="OSE Logo"
