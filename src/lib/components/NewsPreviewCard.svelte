137318c47707e38c418f21df3d0b6e917a6abdb1
04nada <114312907+04nada@users.noreply.github.com>
2025-07-04 17:52:33 +0800

    feat: add News Preview cards

---
+++ src/lib/components/NewsPreviewCard.svelte
@@ -0,0 +1,15 @@
+<script lang=ts>
+    let {
+        src = "",
+        alt = "News Preview Card image.",
+        text = "Hello UPD",
+    } = $props();
+</script>
+
+<div class="bg-blue-100 flex flex-col gap-5 rounded">
+    <img {src} {alt} class="aspect-square object-scale-down "/>
+    
+    <div>
+        {text}
+    </div>
+</div>
--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,5 +1,20 @@
 <script lang=ts>
     import Nav from "$lib/components/Nav.svelte";
+	import NewsPreviewCard from "$lib/components/NewsPreviewCard.svelte";
+
+    let { numCards = 3 } = $props();
 </script>
 
 <Nav />
+
+<!-- todo: only show {numCards} items, using an index variable in the script -->
+<!-- reimplement this with shadcn -->
+<div class="grid grid-cols-{numCards} gap-5 max-w-sm">
+    <NewsPreviewCard src="/ose-bg.png" />
+    <NewsPreviewCard src="/invalid.png" alt="sample invalid image"/>
+    <NewsPreviewCard src="/ose-bg.png" text="Goodbye." />
+    <NewsPreviewCard src="/ose-bg.png" />
+    <NewsPreviewCard src="/ose-bg.png" text="Lorem ipsum dolor sit amet consectetur adapiscing elit." />
+    <NewsPreviewCard src="/ose-bg.png" />
+    <NewsPreviewCard src="/ose-bg.png" />
+</div>
