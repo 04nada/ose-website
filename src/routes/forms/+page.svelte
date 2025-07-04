3ca496ba686959a401e9f99e3ec05f50f377b58e
jslozada1221 <jslozada@up.edu.ph>
2025-07-04 14:39:01 +0800

    added shadcn tabs

--- src/routes/forms/+page.svelte
+++ src/routes/forms/+page.svelte
@@ -24,51 +24,71 @@
 
 <Nav />
  
-<div class="flex w-full max-w-sm flex-col gap-6">
-    <Tabs.Root value="account">
-        <Tabs.List>
-            <Tabs.Trigger value="account">Account</Tabs.Trigger>
-            <Tabs.Trigger value="password">Password</Tabs.Trigger>
+<div class="flex w-[720px] flex-col gap-6 mx-auto mt-5 font-display">
+    <Tabs.Root value="clearanceApplicationForm">
+        <Tabs.List class="mx-auto">
+            <Tabs.Trigger value="clearanceApplicationForm">Online Clearance Application Form</Tabs.Trigger>
+            <Tabs.Trigger value="hardCopyOfDocuments">Request for Hardcopy of Documents</Tabs.Trigger>
         </Tabs.List>
-        <Tabs.Content value="account">
-        <Card.Root>
+        <Tabs.Content value="clearanceApplicationForm">
+        <Card.Root class="flex w-auto">
             <Card.Header>
-                <Card.Title>Account</Card.Title>
+                <Card.Title>Online Clearance Application Form</Card.Title>
                 <Card.Description>
-                Make changes to your account here. Click save when you&apos;re done.
+                Nani ga sukiii?
                 </Card.Description>
             </Card.Header>
             <Card.Content class="grid gap-6">
-            <div class="grid gap-3">
+            <!-- <div class="grid gap-3">
                 <Label for="tabs-demo-name">Name</Label>
                 <Input id="tabs-demo-name" value="Pedro Duarte" />
             </div>
             <div class="grid gap-3">
                 <Label for="tabs-demo-username">Username</Label>
                 <Input id="tabs-demo-username" value="@peduarte" />
+            </div> -->
+            <div>
+                <iframe
+                src={forms['oseclearance'].embedSrc}
+                width=640 height=500 
+                frameborder="0" marginheight="0" marginwidth="0"
+                title={forms['oseclearance'].title} 
+            >
+                Loading...
+            </iframe>
             </div>
             </Card.Content>
-            <Card.Footer>
+            <!-- <Card.Footer>
                 <Button>Save changes</Button>
-            </Card.Footer>
+            </Card.Footer> -->
         </Card.Root>
         </Tabs.Content>
-        <Tabs.Content value="password">
+        <Tabs.Content value="hardCopyOfDocuments">
             <Card.Root>
                 <Card.Header>
-                    <Card.Title>Password</Card.Title>
+                    <Card.Title>Request for Hardcopy of Documents</Card.Title>
                     <Card.Description>
-                    Change your password here. After saving, you&apos;ll be logged out.
+                    Choko Minto! Yori mo anata!?    
                     </Card.Description>
                 </Card.Header>
                 <Card.Content class="grid gap-6">
-                <div class="grid gap-3">
+                <!-- <div class="grid gap-3">
                     <Label for="tabs-demo-current">Current password</Label>
                     <Input id="tabs-demo-current" type="password" />
                 </div>
                 <div class="grid gap-3">
                     <Label for="tabs-demo-new">New password</Label>
                     <Input id="tabs-demo-new" type="password" />
+                </div> -->
+                <div>
+                    <iframe
+                    src={forms['osedocumentrequest'].embedSrc}
+                    width=640 height=500
+                    frameborder="0" marginheight="0" marginwidth="0"
+                    title={forms['osedocumentrequest'].title}
+                >
+                    Loading...
+                </iframe>
                 </div>
                 </Card.Content>
                 <Card.Footer>
