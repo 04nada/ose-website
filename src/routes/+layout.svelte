148e2a062522438c45599538a420e9aa501f9e1d
EdzoAcyatan <csacyatan1@up.edu.ph>
2025-07-07 07:57:13 +0800

    fix: forms title name as requested

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,12 +8,8 @@
   class="min-h-screen w-full bg-cover bg-center bg-fixed relative"
   style="background-image: url('/ose-bg.png')"
 >
-  <div class="pt-5 pl-25">
-    <img
-      src="/ose-logo.png"
-      alt="OSE Logo"
-      class="w-200 h-auto object-contain"
-    />
-  </div>
-  {@render children()}
+	<div class="pt-5 pl-25">
+		<img src="/ose-logo.png" alt="OSE Logo" class="h-auto w-200 object-contain" />
+	</div>
+	{@render children()}
 </div>
--- src/routes/forms/+page.svelte
+++ src/routes/forms/+page.svelte
@@ -1,14 +1,19 @@
-<script lang=ts>
+<script lang='ts'>
     import Nav from "$lib/components/Nav.svelte";
+    import * as Tabs from "$lib/components/ui/tabs/index.js";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
 
     const forms = {
         oseclearance: {
-            title: 'Online Clearance Application Form',
+            title: 'Softcopy of OSE Clearance Application',
             embedSrc: 'https://docs.google.com/forms/d/e/1FAIpQLScFg0BzPqylk_lMs39rOCuZfoUJrf1e671Bbew7i1tA6eqOGA/viewform?embedded=true',
             publicLink: 'https://bit.ly/oseclearance'
         },
         osedocumentrequest: {
-            title: 'Request for Hard Copy of Documents',
+            title: 'Hardcopy Request of Other Documents',
             embedSrc: 'https://docs.google.com/forms/d/e/1FAIpQLSfjaaLJXyCxvJsoPg7xMFh8MtqhMGLg_NYiFCmtdR_YFhMPbg/viewform?embedded=true',
             publicLink: 'https://bit.ly/osedocumentrequest'
         }
@@ -17,8 +22,85 @@
     let selectedForm = $state('oseclearance');
 </script>
 
+<!-- Softcopy of OSE Clearance Application-->
+
 <Nav />
-<div class="flex flex-row center justify-evenly items-center mt-10">
+<div class="flex w-[720px] flex-col gap-6 mx-auto mt-5 font-display">
+    <Tabs.Root value="clearanceApplicationForm">
+        <Tabs.List class="mx-auto">
+            <Tabs.Trigger value="clearanceApplicationForm">Softcopy of OSE Clearance Application</Tabs.Trigger>
+            <Tabs.Trigger value="hardCopyOfDocuments">Hardcopy Request of Other Documents</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="clearanceApplicationForm">
+        <Card.Root class="flex w-auto">
+            <Card.Header>
+                <Card.Title>Softcopy of OSE Clearance Application</Card.Title>
+                <Card.Description>
+                Nani ga sukiii?
+                </Card.Description>
+            </Card.Header>
+            <Card.Content class="grid gap-6">
+            <!-- <div class="grid gap-3">
+                <Label for="tabs-demo-name">Name</Label>
+                <Input id="tabs-demo-name" value="Pedro Duarte" />
+            </div>
+            <div class="grid gap-3">
+                <Label for="tabs-demo-username">Username</Label>
+                <Input id="tabs-demo-username" value="@peduarte" />
+            </div> -->
+            <div>
+                <iframe
+                src={forms['oseclearance'].embedSrc}
+                width=640 height=500 
+                frameborder="0" marginheight="0" marginwidth="0"
+                title={forms['oseclearance'].title} 
+            >
+                Loading...
+            </iframe>
+            </div>
+            </Card.Content>
+            <!-- <Card.Footer>
+                <Button>Save changes</Button>
+            </Card.Footer> -->
+        </Card.Root>
+        </Tabs.Content>
+        <Tabs.Content value="hardCopyOfDocuments">
+            <Card.Root>
+                <Card.Header>
+                    <Card.Title>Hardcopy Request of Other Documents</Card.Title>
+                    <Card.Description>
+                    Choko Minto! Yori mo anata!?    
+                    </Card.Description>
+                </Card.Header>
+                <Card.Content class="grid gap-6">
+                <!-- <div class="grid gap-3">
+                    <Label for="tabs-demo-current">Current password</Label>
+                    <Input id="tabs-demo-current" type="password" />
+                </div>
+                <div class="grid gap-3">
+                    <Label for="tabs-demo-new">New password</Label>
+                    <Input id="tabs-demo-new" type="password" />
+                </div> -->
+                <div>
+                    <iframe
+                    src={forms['osedocumentrequest'].embedSrc}
+                    width=640 height=500
+                    frameborder="0" marginheight="0" marginwidth="0"
+                    title={forms['osedocumentrequest'].title}
+                >
+                    Loading...
+                </iframe>
+                </div>
+                </Card.Content>
+                <Card.Footer>
+                    <Button>Save password</Button>
+                </Card.Footer>
+            </Card.Root>
+        </Tabs.Content>
+    </Tabs.Root>
+</div>
+
+<div class="flex flex-row center justify-evenly items-center">
     <div class="flex flex-col width-restriction space-y-10">
         <h1 class = "header merriweather-bold">
             Forms
