574551d60937167d1a538e9b6370ab73fe3a46ea
NikYT05 <rvtan1@up.edu.ph>
2025-07-08 09:07:51 +0800

    feat: shadcn added center tabs and resources

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,5 +1,8 @@
 <script lang=ts>
     import Nav from "$lib/components/Nav.svelte";
+    import * as Tabs from "$lib/components/ui/tabs/index.js";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
 
     const resources = {
         studentconduct2012: {
@@ -13,6 +16,36 @@
 </script>
 
 <Nav />
+
+<div class="flex w-[720px] flex-col gap-6 mx-auto mt-5 font-display">
+    <Tabs.Root value="codeOfStudentConduct">
+        <Tabs.List class="mx-auto">
+            <Tabs.Trigger value="codeOfStudentConduct">2012 Code of Student Conduct</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="codeOfStudentConduct">
+        <Card.Root class="flex-auto">
+            <Card.Header>
+                <Card.Title>2012 Code of Student Conduct</Card.Title>
+                <Card.Description>
+                    OSE 太好了！
+                </Card.Description>
+            </Card.Header>
+            <Card.Content class="grid gap-6">
+            <div>
+                <iframe
+                src={resources['studentconduct2012'].embedSrc}
+                width=640 height=500 
+                frameborder="0" marginheight="0" marginwidth="0"
+                title={resources['studentconduct2012'].title} 
+            >
+                Loading...
+            </iframe>
+            </div>
+            </Card.Content>
+        </Card.Root>
+        </Tabs.Content>
+    </Tabs.Root>
+</div>
 
 <div class="flex flex-row center justify-evenly items-center">
     <div class="flex flex-col width-restriction space-y-10">
