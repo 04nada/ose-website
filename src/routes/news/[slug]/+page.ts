fe6f5545d565599e160fc9561f95fa9f37123be5
04nada <114312907+04nada@users.noreply.github.com>
2025-07-16 15:47:21 +0800

    feat: fetch news page data from WordPress

--- src/routes/news/[slug]/+page.ts
+++ src/routes/news/[slug]/+page.ts
@@ -1,11 +1,35 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = ({ params }) => {
-	if (params.slug === 'first-announcement') {
-		return {
-			title: 'This is the Office for Student Ethics.',
-			content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
+const queryPostIDs = `{
+    newsPosts {
+        nodes {
+            newsPostFields {
+                title
+                page
+                datePublished
+                dateModified
+                content
+                previewImage {
+                    node {
+                        altText
+                        mediaItemUrl
+                    }
+                }
+            }
+        }
+    }
+}
+`;
+
+const databaseUrl = new URL(import.meta.env.VITE_REST_API_URL);
+databaseUrl.search = new URLSearchParams({query: queryPostIDs}).toString();
+
+export const load: PageLoad = async ({ params, fetch }) => {
+    if (params.slug === 'test') {
+        return {
+            title: 'This is the Office for Student Ethics.',
+            content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
             date_created: '07 July 2025',
             date_modified: '08 July 2025',
             images: [
@@ -18,8 +42,37 @@ export const load: PageLoad = ({ params }) => {
                     alt: 'Padayon 2025',
                 },
             ],
-		};
-	}
+        };
+    }
 
-	error(404, 'Not found');
+    const response = await fetch(databaseUrl);
+    if (response.ok) {
+        const responseObj = await response.json();
+        const posts = responseObj.data.newsPosts.nodes;
+
+        for (const item of posts) {
+            if (params.slug === item.newsPostFields.page) {
+                console.log('found: ', params.slug);
+                console.log('item: ', item);
+                return {
+                    title: item.newsPostFields.title,
+                    content: item.newsPostFields.content,
+                    datePublished: Date.parse(item.newsPostFields.datePublished),
+                    dateModified: Date.parse(item.newsPostFields.dateModified),
+                    images: [
+                        {
+                            src: '/news1.jpg',
+                            alt: 'Notice to the Public',
+                        },
+                        {
+                            src: '/news2.jpg',
+                            alt: 'Padayon 2025',
+                        },
+                    ],
+                };
+            }
+        };
+    }
+
+    error(404, 'Not found');
 };
