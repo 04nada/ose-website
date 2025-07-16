888c4248aec1cc9028bc9a3c10e6b883f4759f63
sojaja <janmend3@gmail.com>
2025-07-16 15:35:11 +0800

    feature: individual news page layout

--- src/routes/news/[slug]/+page.svelte
+++ src/routes/news/[slug]/+page.svelte
@@ -1,25 +1,51 @@
 <script lang="ts">
 	import Nav from '$lib/components/Nav.svelte';
 	import type { PageProps } from './$types';
+    import * as Carousel from "$lib/components/ui/carousel/index.js";
 
-	let { data }: PageProps = $props();
+    let { data }: PageProps = $props();
 </script>
+
 
 <Nav />
 
-<div>
-    <h1>{data.title}</h1>
+<div class="text-white">
+    <h1 class="merriweather-semibold mt-10 ml-20 text-5xl">
+    {data.title}
+    </h1>
+    
 
-    <h2>Created: {data.date_created}</h2>
-    <h2>Last Modified: {data.date_modified}</h2>
+    <h2 class="ml-20 mt-3 text-sm">
+        Created: {data.date_created}</h2>
+    <h2 class="ml-20 text-sm">
+        Last Modified: {data.date_modified}</h2>
 
-    <div>
-        <p>{data.content}</p>
-    </div>
+   <div class="flex justify-center mt-5 overflow-x-visible">
+    <Carousel.Root class="w-full max-w-5xl px-8 relative">
+        <Carousel.Content class="flex gap-4 -mx-4">
+            {#each data.images as { src, alt }, i (i)}
+                <Carousel.Item class="basis-[80%] shrink-0">
+                    <div class="h-[450px] w-full overflow-hidden rounded-lg bg-white shadow-md flex justify-center items-center">
+                        <img
+                            src={src}
+                            alt={alt}
+                            class="max-h-full max-w-full object-contain rounded-md"
+                        />
+                    </div>
+                </Carousel.Item>
+            {/each}
+        </Carousel.Content>
 
-    <div>
-        {#each data.images as {src, alt}}
-            <img {src} {alt} width="500px"/>
-        {/each}
-    </div>
+        <Carousel.Previous class="absolute left-0 top-1/2 -translate-y-1/2 z-10" />
+        <Carousel.Next class="absolute right-0 top-1/2 -translate-y-1/2 z-10" />
+    </Carousel.Root>
 </div>
+
+
+    <div>
+        <p class="p-paragraph mt-10 ml-20 mr-20 text-base">{data.content}</p>
+    </div>
+
+</div>
+
+
--- src/routes/news/[slug]/+page.ts
+++ src/routes/news/[slug]/+page.ts
@@ -5,7 +5,7 @@ export const load: PageLoad = ({ params }) => {
 	if (params.slug === 'first-announcement') {
 		return {
 			title: 'This is the Office for Student Ethics.',
-			content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
+			content: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Lorem ipsum dolor sit amet consectetur adipiscing elit.Lorem ipsum dolor sit amet consectetur adipiscing elit.Lorem ipsum dolor sit amet consectetur adipiscing elit.Lorem ipsum dolor sit amet consectetur adipiscing elit.Lorem ipsum dolor sit amet consectetur adipiscing elit. Lorem ipsum dolor sit amet consectetur adipiscing elit. Lorem ipsum dolor sit amet consectetur adipiscing elit. Lorem ipsum dolor sit amet consectetur adipiscing elit. Lorem ipsum dolor sit amet consectetur adipiscing elit.',
             date_created: '07 July 2025',
             date_modified: '08 July 2025',
             images: [
