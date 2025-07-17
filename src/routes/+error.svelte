5054f997020e3f4e751a2a49e0cafa9cdcbc5558
04nada <114312907+04nada@users.noreply.github.com>
2025-07-17 15:26:02 +0800

    feat: make new default error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang=ts>
+	import { page } from "$app/state";
+    import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<Nav />
+
+<h2> Error {page.status} </h2>
+<div>
+    <p>
+        {page.error?.message}
+    </p>
+
+    <div>
+        Return <a href="/">Home</a>
+    </div>
+</div>
