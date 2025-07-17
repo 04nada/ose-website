a4cfac34f48ccaa6c209cb23461cff326ac0b462
04nada <114312907+04nada@users.noreply.github.com>
2025-07-17 15:27:32 +0800

    feat: make error page for news pages

---
+++ src/routes/news/[slug]/+error.svelte
@@ -0,0 +1,21 @@
+<script lang=ts>
+	import { page } from "$app/state";
+    import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<Nav />
+
+<h2> Error {page.status} </h2>
+<div>
+    <p>
+        {page.error?.message}
+    </p>
+
+    <p>
+        This news article does not exist.
+    </p>
+
+    <div>
+        Return <a href="/">Home</a>
+    </div>
+</div>
