ba977d88da2d186470fefe557399f54ad2917d2d
NikYT05 <rvtan1@up.edu.ph>
2025-07-18 18:21:11 +0800

    style: Error Page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,13 +5,9 @@
 
 <Nav />
 
-<h2> Error {page.status} </h2>
-<div>
-    <p>
-        {page.error?.message}
-    </p>
 
-    <div>
-        Return <a href="/">Home</a>
-    </div>
+<div class="text-xl text-white font-display pb-4 m-10 p-2 border-l border-[#EDAE1A] grid justify-center md:text-3xl md:m-20 md:p-5 md:ml-0 md:pr-0 lg:ml-45 lg:pr-40">
+    <h2 class="grid text-white pb-4 md:pb-10"> Page Not Found </h2>
+    We couldn’t find the content you were looking for. Please check the URL or explore our navigation bar to find what you need.
+
 </div>
