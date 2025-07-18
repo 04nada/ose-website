097aa38b5accdf2865b4eec990d34a4dc981f006
04nada <114312907+04nada@users.noreply.github.com>
2025-07-18 12:51:01 +0800

    style: increase tab size

--- src/lib/components/NavItem.svelte
+++ src/lib/components/NavItem.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-  import { page } from '$app/state';
+    import { page } from '$app/state';
 
-  let className = '';
-  export { className as class };
-  export let navName: string;
-  export let route: string;
+    let className = '';
+    export { className as class };
+    export let navName: string;
+    export let route: string;
 
-  $: isActive = route === '/'
-    ? page.url.pathname === '/'
-    : page.url.pathname.startsWith(route);
+    $: isActive = route === '/'
+        ? page.url.pathname === '/'
+        : page.url.pathname.startsWith(route);
 </script>
 
 <div class="relative">
-  <a class="{className} flex flex-row font-display" href={route}>
-    <p class="sm:font-medium text-textcolor">{navName}</p>
-  </a>
+    <a class="{className} flex flex-row font-display" href={route}>
+        <p class="sm:font-medium text-textcolor">{navName}</p>
+    </a>
 
-  {#if isActive}
-    <span
-      class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[100%] h-[1.5px] bg-[#EDAE1A]"
-    ></span>
-  {/if}
+    {#if isActive}
+        <span
+            class="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[100%] h-[1.5px] bg-[#EDAE1A]"
+        ></span>
+    {/if}
 </div>
