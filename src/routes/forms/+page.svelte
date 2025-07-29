15cf7e09032ea30e8419452d01f888c41c5e5bc5
NikYT05 <rvtan1@up.edu.ph>
2025-07-29 11:43:57 +0800

    feat: Different Mobile and Desktop Views for Forms Page

--- src/routes/forms/+page.svelte
+++ src/routes/forms/+page.svelte
@@ -26,124 +26,102 @@
 
 <h1 class="text-3xl merriweather-bold mt-5 ml-30 text-white"> Forms </h1>
 
-<div class="flex w-[720px] flex-col gap-6 mx-auto mt-5 font-display">
-    <Tabs.Root value="clearanceApplicationForm">
-        <Tabs.List class="mx-auto">
-            <Tabs.Trigger value="clearanceApplicationForm">Softcopy of OSE Clearance Application</Tabs.Trigger>
-            <Tabs.Trigger value="hardCopyOfDocuments">Hardcopy Request of Other Documents</Tabs.Trigger>
-        </Tabs.List>
-        
-        <Tabs.Content value="clearanceApplicationForm">
-            <Card.Root class="flex w-auto">
-                <Card.Header>
-                    <Card.Title>Softcopy of OSE Clearance Application</Card.Title>
-                    <Card.Description>
-                    Nani ga sukiii?
-                    </Card.Description>
-                </Card.Header>
+<div class="hidden sm:block">
+    <div class="flex w-[720px] flex-col gap-6 mx-auto mt-5 font-display">
+        <Tabs.Root value="clearanceApplicationForm">
+            <Tabs.List class="mx-auto">
+                <Tabs.Trigger value="clearanceApplicationForm">Softcopy of OSE Clearance Application</Tabs.Trigger>
+                <Tabs.Trigger value="hardCopyOfDocuments">Hardcopy Request of Other Documents</Tabs.Trigger>
+            </Tabs.List>
+            
+            <Tabs.Content value="clearanceApplicationForm">
+                <Card.Root class="flex w-auto">
+                    <Card.Header>
+                        <Card.Title>Softcopy of OSE Clearance Application</Card.Title>
+                        <Card.Description>
+                        Nani ga sukiii?
+                        </Card.Description>
+                    </Card.Header>
 
-                <Card.Content class="grid gap-6">
-                    <!-- <div class="grid gap-3">
-                        <Label for="tabs-demo-name">Name</Label>
-                        <Input id="tabs-demo-name" value="Pedro Duarte" />
-                    </div>
-                    <div class="grid gap-3">
-                        <Label for="tabs-demo-username">Username</Label>
-                        <Input id="tabs-demo-username" value="@peduarte" />
-                    </div> -->
-                    <div>
-                        <iframe
-                        src={forms['oseclearance'].embedSrc}
-                        width=640 height=500 
-                        frameborder="0" marginheight="0" marginwidth="0"
-                        title={forms['oseclearance'].title} 
-                    >
-                        Loading...
-                    </iframe>
-                    </div>
-                </Card.Content>
-
-                <!-- <Card.Footer>
-                    <Button>Save changes</Button>
-                </Card.Footer> -->
-            </Card.Root>
-        </Tabs.Content>
-        
-        <Tabs.Content value="hardCopyOfDocuments">
-            <Card.Root>
-                <Card.Header>
-                    <Card.Title>Hardcopy Request of Other Documents</Card.Title>
-
-                    <Card.Description>
-                    Choko Minto! Yori mo anata!?    
-                    </Card.Description>
-                </Card.Header>
-
-                <Card.Content class="grid gap-6">
-                    <!-- <div class="grid gap-3">
-                        <Label for="tabs-demo-current">Current password</Label>
-                        <Input id="tabs-demo-current" type="password" />
-                    </div>
-                    <div class="grid gap-3">
-                        <Label for="tabs-demo-new">New password</Label>
-                        <Input id="tabs-demo-new" type="password" />
-                    </div> -->
-                    <div>
-                        <iframe
-                            src={forms['osedocumentrequest'].embedSrc}
-                            width=640 height=500
+                    <Card.Content class="grid gap-6">
+                        <!-- <div class="grid gap-3">
+                            <Label for="tabs-demo-name">Name</Label>
+                            <Input id="tabs-demo-name" value="Pedro Duarte" />
+                        </div>
+                        <div class="grid gap-3">
+                            <Label for="tabs-demo-username">Username</Label>
+                            <Input id="tabs-demo-username" value="@peduarte" />
+                        </div> -->
+                        <div>
+                            <iframe
+                            src={forms['oseclearance'].embedSrc}
+                            width=640 height=500 
                             frameborder="0" marginheight="0" marginwidth="0"
-                            title={forms['osedocumentrequest'].title}
+                            title={forms['oseclearance'].title} 
                         >
                             Loading...
                         </iframe>
-                    </div>
-                </Card.Content>
+                        </div>
+                    </Card.Content>
 
-                <Card.Footer>
-                    <Button>Save password</Button>
-                </Card.Footer>
-            </Card.Root>
-        </Tabs.Content>
-    </Tabs.Root>
+                    <!-- <Card.Footer>
+                        <Button>Save changes</Button>
+                    </Card.Footer> -->
+                </Card.Root>
+            </Tabs.Content>
+            
+            <Tabs.Content value="hardCopyOfDocuments">
+                <Card.Root>
+                    <Card.Header>
+                        <Card.Title>Hardcopy Request of Other Documents</Card.Title>
+
+                        <Card.Description>
+                        Choko Minto! Yori mo anata!?    
+                        </Card.Description>
+                    </Card.Header>
+
+                    <Card.Content class="grid gap-6">
+                        <!-- <div class="grid gap-3">
+                            <Label for="tabs-demo-current">Current password</Label>
+                            <Input id="tabs-demo-current" type="password" />
+                        </div>
+                        <div class="grid gap-3">
+                            <Label for="tabs-demo-new">New password</Label>
+                            <Input id="tabs-demo-new" type="password" />
+                        </div> -->
+                        <div>
+                            <iframe
+                                src={forms['osedocumentrequest'].embedSrc}
+                                width=640 height=500
+                                frameborder="0" marginheight="0" marginwidth="0"
+                                title={forms['osedocumentrequest'].title}
+                            >
+                                Loading...
+                            </iframe>
+                        </div>
+                    </Card.Content>
+
+                    <Card.Footer>
+                        <Button>Save password</Button>
+                    </Card.Footer>
+                </Card.Root>
+            </Tabs.Content>
+        </Tabs.Root>
+    </div>
 </div>
 
-<div class="flex sm:hidden lex-row center justify-evenly items-center">
+<div class="block flex sm:hidden lex-row center justify-evenly items-center">
     <div class="flex flex-col width-restriction space-y-10">
         <h1 class = "header merriweather-bold">
             Forms
         </h1>
 
         {#each Object.entries(forms) as [id, form]}
-            <button class="styled-buttons poppins-light" aria-current={selectedForm === id}
-                onclick={() => selectedForm = id}
+            <button class="styled-buttons poppins-light m-4" 
+                onclick={() => window.open(form.embedSrc.replace('?embedded=true', ''), '_blank')}
             >
                 {form.title}
             </button>
         {/each}
-    </div>
-    
-    <div>
-        {#if selectedForm === 'oseclearance'}
-            <iframe
-                src={forms[selectedForm].embedSrc}
-                width=640 height=500
-                frameborder="0" marginheight="0" marginwidth="0"
-                title={forms[selectedForm].title}
-            >
-                Loading...
-            </iframe>
-        {:else if selectedForm === 'osedocumentrequest'}
-            <iframe
-                src={forms[selectedForm].embedSrc}
-                width=640 height=500
-                frameborder="0" marginheight="0" marginwidth="0"
-                title={forms[selectedForm].title}
-            >
-                Loading...
-            </iframe>
-        {:else}
-            <p> Invalid form selected. </p>
-        {/if}
     </div>
 </div>
